7d47c1392fbd40fa659556098df8aec5722cd06c
Philip Gurr <philipgurr10@gmail.com>
2019-09-05 21:22:33 +0200

    Refactoring

--- app/src/main/java/com/philipgurr/kprefexample/MainActivity.kt
+++ app/src/main/java/com/philipgurr/kprefexample/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.philipgurr.kprefexample
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.philipgurr.kpref.ContextProvider
 import com.philipgurr.kpref.SharedPreference
+import com.philipgurr.kpref.editPreferences
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), ContextProvider {
@@ -30,10 +31,15 @@ class MainActivity : AppCompatActivity(), ContextProvider {
 
         // Save values on button press
         save.setOnClickListener {
-            val user = User(name.text.toString(), surname.text.toString(), age.text.toString().toInt())
+            val user =
+                User(name.text.toString(), surname.text.toString(), age.text.toString().toInt())
             userPref = user
 
-            Toast.makeText(this, "User saved. Restart the app to reload from preferences.", Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                this,
+                "User saved. Restart the app to reload from preferences.",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
@@ -41,14 +47,18 @@ class MainActivity : AppCompatActivity(), ContextProvider {
         super.onResume()
 
         // Show values from preferences
-        if(userPref != defaultUser) {
+        if (userPref != defaultUser) {
             name.setText(userPref.name)
             surname.setText(userPref.surname)
             age.setText(userPref.age.toString())
         }
     }
+
+    data class User(
+        val name: String,
+        val surname: String,
+        val age: Int
+    )
 }
 
-data class User(val name: String,
-                val surname: String,
-                val age: Int)
+
